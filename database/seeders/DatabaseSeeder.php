d530780e692d8551a41666e4398b021ac54eb653
bowo rahayu <rahayuributwibowo@gmail.com>
2026-02-11 14:28:24 +0700

    Update product types in DatabaseSeeder
    - Change the `type` attribute from 'drink' to 'single' for beverage products in the seeder data.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Option;
+use App\Models\Product;
+use App\Models\Restaurant;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
-use App\Models\Restaurant;
-use App\Models\Category;
-use App\Models\Product;
-use App\Models\Option;
 
 class DatabaseSeeder extends Seeder
 {
@@ -87,35 +87,35 @@ class DatabaseSeeder extends Seeder
             ],
             [
                 'id' => 4, 'cat' => 'minuman', 'name' => 'Kopi Susu Aren', 'price' => 11000, 'desc' => 'Signature coffee dengan gula aren asli.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/kopi-susu-aren.jpeg'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/kopi-susu-aren.jpeg'
             ],
             [
                 'id' => 5, 'cat' => 'minuman', 'name' => 'Blue Ocean', 'price' => 11000, 'desc' => 'Kesegaran soda biru lemon yang unik.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/blue-ocean.jpeg'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/blue-ocean.jpeg'
             ],
             [
                 'id' => 6, 'cat' => 'minuman', 'name' => 'Milky Mango', 'price' => 11000, 'desc' => 'Creamy milk dengan rasa mangga manis.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/milky-mango.jpeg'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/milky-mango.jpeg'
             ],
             [
                 'id' => 7, 'cat' => 'minuman', 'name' => 'Passion Soda', 'price' => 11000, 'desc' => 'Soda markisa yang menyegarkan dahaga.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/passion-soda.jpeg'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/passion-soda.jpeg'
             ],
             [
                 'id' => 8, 'cat' => 'minuman', 'name' => 'Choco Milky', 'price' => 11000, 'desc' => 'Susu cokelat creamy yang nyoklat banget.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/choco-milky.jpeg'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/choco-milky.jpeg'
             ],
             [
                 'id' => 9, 'cat' => 'minuman', 'name' => 'Yakult Mango', 'price' => 11000, 'desc' => 'Perpaduan Yakult dan mangga yang segar.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/yakult-mango.jpeg'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/yakult-mango.jpeg'
             ],
             [
                 'id' => 10, 'cat' => 'minuman', 'name' => 'Yakult Peach', 'price' => 11000, 'desc' => 'Kesegaran Yakult dengan aroma peach.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/yakult-peach.jpeg'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/yakult-peach.jpeg'
             ],
             [
                 'id' => 11, 'cat' => 'minuman', 'name' => 'Cendol Aren', 'price' => 10000, 'desc' => 'Cita rasa tradisional cendol gula aren.',
-                'type' => 'drink', 'options' => [], 'img' => 'products/1/cendol-aren.png'
+                'type' => 'single', 'options' => [], 'img' => 'products/1/cendol-aren.png'
             ]
         ];
 
