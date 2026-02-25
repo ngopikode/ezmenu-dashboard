526984fcba520fc02b404a60c7e6a52cbf447fd4
bowo rahayu <rahayuributwibowo@gmail.com>
2026-02-25 22:59:29 +0700

    Refactor showProductPreview in MenuController
    - Remove `int` type hint from `$productId` parameter.
    - Use injected `$request` object instead of global helper for protocol check.
    - Remove unnecessary blank line.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -14,14 +14,14 @@ class MenuController extends Controller
      * Show a product preview for social media bots.
      *
      * @param Request $request
-     * @param int $productId
+     * @param string $subdomain
+     * @param string $productId
      * @return View
      */
-    public function showProductPreview(Request $request, $productId): View
+    public function showProductPreview(Request $request, string $subdomain, string $productId): View
     {
         /** @var Restaurant $restaurant */
         $restaurant = $request->restaurant;
-        dd($productId);
 
         $product = Product::where('restaurant_id', $restaurant->id)->findOrFail($productId);
 
@@ -29,7 +29,7 @@ class MenuController extends Controller
         $reactAppBaseUrl = config('app.frontend_url_base');
 
         $protocol = $request->isSecure() ? 'https' : 'http';
-        $fullReactUrl = "{$protocol}://{$restaurant->subdomain}.{$reactAppBaseUrl}";
+        $fullReactUrl = "$protocol://$subdomain.$reactAppBaseUrl";
         $imageUrl = $product->image ? "$fullReactUrl/" . asset(Storage::url($product->image)) : null;
 
         return view('product_preview', [
